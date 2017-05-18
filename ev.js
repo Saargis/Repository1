646de20f4fafbe630b768d9eabdf856a154cd439
Saargis <sargis2122vahe@gmail.com>
2017-05-18 20:36:03 +0400

    intervalCounter

--- ev.js
+++ ev.js
@@ -1,6 +1,19 @@
 'use strict';
 
-setInterval(function()
+let counter = 0;
+
+// setInterval(function()
+// {
+//   console.log(`Called ${++counter} times`);
+//   if(counter < 7)
+//   {
+//
+//   }else{
+//     clearInterval;
+//   }
+// }, 3000)
+
+process.stdin.on('data', function(d)
 {
-  console.log('Hello Karen :)');
-}, 3000)
+  console.log(d.toString().toUpperCase())
+})
