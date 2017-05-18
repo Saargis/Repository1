f4bedc63d048f7ee6661924e65a47ef70991f986
Saargis <sargis2122vahe@gmail.com>
2017-05-18 19:12:01 +0400

    newForLoop

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 
 
-function speak() {
-  console.log('Hello');
+const speak = function() {
+  const arr = ['foo', 'bar', 'baz'];
+  for(const i of arr)
+  {
+    console.log(i.toUpperCase());
+  }
 }
 
 speak();
